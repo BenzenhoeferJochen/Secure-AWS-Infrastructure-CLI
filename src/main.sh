2bc62632f550223257056d87e53d2a3c54d5e2b5
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-05-23 14:41:20 +0200

    print aoutput of child shell to the output

--- src/main.sh
+++ src/main.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 bash login/login.sh 
-vpc_id=$(bash vpc/create_vpc.sh)
+vpc_id=$(bash vpc/create_vpc.sh | tee /dev/tty)| tail -1
+private_route_table_id=$(bash route_table/create_route_table.sh $vpc_id "Private Route Table")
+public_route_table_id=$(bash route_table/create_route_table.sh $vpc_id "Public Route Table")
+
