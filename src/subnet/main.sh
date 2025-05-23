dc1629c0a622a7be74092db5e9474beca912a2fc
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-05-23 16:21:29 +0200

    subnet main show output on console

--- src/subnet/main.sh
+++ src/subnet/main.sh
@@ -6,7 +6,7 @@ read subnets_number
 declare -a subnets
 for i in $(seq 1 $subnets_number)
 do
-    subnets[$((i-1))]=$(bash $script_dir/create_subnet.sh $1)
+    subnets[$((i-1))]=$(bash $script_dir/create_subnet.sh $1 | tee /dev/tty| tail -1)
 	aws ec2 associate-route-table --route-table-id $2 --subnet-id ${subnets[$((i-1))]}
 done
 echo $subnets
