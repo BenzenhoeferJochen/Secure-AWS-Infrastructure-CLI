f4aef2dbbb76b9f3754fa14b10f4ac1086df3650
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-05-23 15:18:23 +0200

    removed some spaces

--- src/login/login.sh
+++ src/login/login.sh
@@ -15,7 +15,7 @@ do
     echo "Wich Return Value Type would you like to use? Default json"
     read aws_output
 	echo -e "[default]\n aws_access_key_id=$aws_access_key_id \n aws_secret_access_key=$aws_secret_access_key \n aws_session_token=$aws_session_token">/home/$(whoami)/.aws/credentials
-	echo -e "[default]\n region = ${aws_region:-"us-west-2"}\n output = ${aws_output:-"json"}">/home/$(whoami)/.aws/config
+	echo -e "[default]\n region=${aws_region:-"us-west-2"}\n output=${aws_output:-"json"}">/home/$(whoami)/.aws/config
     output=$(aws s3 ls 2>&1 >/dev/null)
 done
 echo "Sucessfully connected to AWS"
